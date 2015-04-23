ed718e95264f261ae6678267ac5b59e9e98848aa
tracyfuller <tracy.e.fuller@gmail.com>
2015-04-23 16:43:05 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+	var numClicks = 1;
+		
+
+	$("#generateLines").click(function(){
+		var addLine = "<div>Line #" + numClicks + " <button class='color'>Change Color</button><button class='remove'>Remove</button></div>";
+		$("#lines").append(addLine);
+		numClicks++;
+	});
+
+	$("#lines").on("click", ".remove", function(){
+		$(this).parent("div").remove();
+	});
+
+	$("#lines").on("click", ".color", function(){
+		$(this).parent("div").addClass('colorChange');
+	});
+
+});
---
+++ readme.md
@@ -0,0 +1,12 @@
+##README
+
+####April 23, 2015
+
+This is my assessment on jQuery. There are 5 files:
+*index.html
+*app.js
+*stylesheet.css
+*jquery.min.js
+*readme.md
+
+Enjoy. 
